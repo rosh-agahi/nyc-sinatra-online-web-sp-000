ed43da302f59bd7421b6aac054c68e25fda6a026
Roshanak Agahi <rosh.agahi@gmail.com>
2021-06-17 17:47:21 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,41 @@
 class FiguresController < ApplicationController
   # add controller methods
+  
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+  
+  post '/figures' do
+    @figure = Figure.create(name: params["figure"]["name"])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    
+    redirect '/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = @figure.landmarks
+    @titles = @figure.titles
+    erb :'/figures/edit'
+  end
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+  
 end
